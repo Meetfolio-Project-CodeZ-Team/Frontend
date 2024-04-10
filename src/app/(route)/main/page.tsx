cdb5f67058db57b610f89f118f2a8cc18173211a
Minkyu0424 <yng1404@gmail.com>
2024-04-10 16:59:15 +0900

    :sparkles:[feat]: 헤더 생성

--- src/app/(route)/main/page.tsx
+++ src/app/(route)/main/page.tsx
@@ -4,11 +4,9 @@ import Header from '@/app/components/layout/Header'
 
 export default function MainPage() {
   return (
-    <section className="flex flex-col items-center min-h-screen">
-      <div className=" w-[1440px] mx-auto">
-        <div className="w-[100%] mx-auto">
-          <Header />
-        </div>
+    <section className="flex flex-col items-center w-[1440px] mx-auto min-h-screen border-b border-slate-600">
+      <div className="w-[1440px] mx-auto">
+        <Header />
       </div>
     </section>
   )
--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -7,26 +7,36 @@ interface HeaderProps {
 
 const Header = () => {
   return (
-    <header className="w-full h-[80px] flex items-center min-w-[800px] mx-[60px] gap-x-[450px] border border-slate-600">
-      <Link
-        className="flex items-center justify-center gap-x-2.5"
-        href={'/main'}
-      >
-        <div className="w-[35px] h-[35px] bg-slate-600 rounded-[100px]"></div>
-        <div className="w-[150x] h-11 text-gray-900 text-[32px] font-semibold leading-[48px]">
-          Meetfolio
+    <div className=" w-full border-b-2 border-[#486284]">
+      <header className="w-full h-[80px] flex items-center content-between min-w-[800px] gap-x-[470px] px-[100px]">
+        <Link
+          className="flex items-center justify-center gap-x-2.5 h-9"
+          href={'/main'}
+        >
+          <div className="w-9 h-9 bg-[#486284] rounded-[100px]"></div>
+          <div className="w-[150x] h-11 text-[32px] font-semibold leading-[48px]">
+            Meetfolio
+          </div>
+        </Link>
+        <div className="flex w-[600px] gap-[30px] h-9 text-[#486284] text-lg cursor-pointer items-center ">
+          <Link href="login">경험분해하기</Link>
+          <Link href="login">AI자기소개서솔루션</Link>
+          <Link href="login">커뮤니티</Link>
+          <div className="flex">
+            <div className="w-[22px] h-[2px] origin-top-left rotate-90 opacity-80 border border-[#486284]"></div>
+            <Link
+              href="login"
+              className="underline underline-offset-1 text-base"
+            >
+              회원가입
+            </Link>
+          </div>
+          <button className="w-[82px] h-[38px] bg-[#486284] text-white text-sm rounded font-medium">
+            로그인
+          </button>
         </div>
-      </Link>
-      <div className="flex gap-[40px] cursor-pointer items-center justify-center">
-        <Link href="login">경험분해하기</Link>
-        <Link href="login">AI자기소개서솔루션</Link>
-        <Link href="login">커뮤니티</Link>
-        <Link href="login">회원가입</Link>
-        <button className='w-[82px] h-[38px] bg-slate-600 rounded"'>
-          로그인
-        </button>
-      </div>
-    </header>
+      </header>
+    </div>
   )
 }
 
