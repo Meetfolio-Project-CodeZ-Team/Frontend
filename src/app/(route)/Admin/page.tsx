bda3a16d68aeed8880559288a65a5d2206281517
Minkyu0424 <yng1404@gmail.com>
2024-04-13 02:41:38 +0900

    ✨ [feat]: 관리자 페이지 초기 라우팅

--- src/app/(route)/Admin/page.tsx
+++ src/app/(route)/Admin/page.tsx
@@ -1,15 +1,18 @@
 'use client'
 
+import AdminNavContainer from '@/app/components/admin/containers/AdminNavContainer'
 import Header from '@/app/components/layout/Header'
 import IntroudeContainer from '@/app/components/main/containers/IntroduceContainer'
 import MainContainer from '@/app/components/main/containers/MainContainer'
 
-export default function MainPage() {
+export default function AdminPage() {
   return (
     <section className="flex flex-col items-center w-full min-h-screen">
-      <Header />
       <div className="w-[1440px] mb-10">
-        <MainContainer />
+        <Header isAdmin={true} />
+        <div className="flex">
+          <AdminNavContainer />
+        </div>
       </div>
       <IntroudeContainer />
     </section>
