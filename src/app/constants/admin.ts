7aaa4f5fd1309944ee2081f9038972107be8cb98
Minkyu0424 <yng1404@gmail.com>
2024-04-13 03:49:33 +0900

    ✨ [feat]: admin const생성

---
+++ src/app/constants/admin.ts
@@ -0,0 +1,12 @@
+export const DASHBOARD = {
+  Service: 'AI 솔루션 서비스 사용량',
+  User: '활성 회원 수',
+  SatisFaction: '서비스 만족도',
+  Profit: '총 매출',
+  Job: '희망 직무 회원 통계',
+  Point: '포인트 통계',
+} as const
+
+export const SERVICE =['자기소개서 피드백', '직무 역량 분석', '총 사용량']
+export const POINT = ['자기소개서 조회', 'AI 직무 역량 분석', '총합']
+
