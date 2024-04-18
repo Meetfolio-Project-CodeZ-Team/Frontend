0b762caae8654992900373c1c0b41e0f966afe98
Minkyu0424 <yng1404@gmail.com>
2024-04-18 23:32:17 +0900

    ✨ [feat]: 대쉬보드type 선언, api 초기설정

--- src/app/(route)/Admin/page.tsx
+++ src/app/(route)/Admin/page.tsx
@@ -2,17 +2,47 @@
 import AdminNavContainer from '@/app/components/admin/containers/AdminNavContainer'
 import DashboardContainer from '@/app/components/admin/containers/DashboardContainer'
 import Header from '@/app/components/layout/Header'
+import { useEffect, useState } from 'react'
 
 export default function AdminPage() {
-  return (
-    <section className="flex flex-col min-h-screen">
-      <Header isAdmin={true} />
-      <div className="flex w-[full] h-[980px]">
-        <AdminNavContainer selected={'dashboard'} />
-        <div className="flex-grow">
-          <DashboardContainer />
+  const [data, setData] = useState<ResponseDashBoard | null>(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_NEXT_SERVER}/api/main`,
+      )
+      const resData = await response.json()
+      console.log('가져온 resData', resData)
+
+      setData(resData.result)
+      // if (resData.tokens) {
+      //   setTokens(
+      //     responseData.tokens.accessToken,
+      //     responseData.tokens.refreshToken,
+      //   )
+      // }
+    }
+    fetchData()
+  }, [])
+  if (data) {
+    const { aiSolutionInfo, memberInfo, pointInfo, paymentInfo } = data
+    console.log(
+      aiSolutionInfo,
+      memberInfo,
+      pointInfo,
+      paymentInfo,
+      '대쉬보드 데이터 가져오기~',
+    )
+    return (
+      <section className="flex flex-col min-h-screen">
+        <Header isAdmin={true} />
+        <div className="flex w-[full] h-[980px]">
+          <AdminNavContainer selected={'dashboard'} />
+          <div className="flex-grow">
+            <DashboardContainer />
+          </div>
         </div>
-      </div>
-    </section>
-  )
+      </section>
+    )
+  }
 }
--- src/app/_types/admin.ts
+++ src/app/_types/admin.ts
@@ -5,3 +5,33 @@ interface ModelValue {
   accuracy: number
   loss: number
 }
+
+interface ResponseDashBoard {
+  aiSolutionInfo: AISolutionTypes
+  memberInfo: memberInfo
+  pointInfo: pointInfoTypes
+  paymentInfo: number
+}
+
+interface AISolutionTypes {
+  feedbackCount: number
+  analysisCount: number
+  totalCount: number
+  satisfaction: number
+}
+
+interface pointInfoTypes {
+  myPoint: number
+  pointList: pointListTypes[]
+  listSize: number
+  totalPage: number
+  totalElements: number
+  isFirst: boolean
+  isLast: boolean
+}
+interface pointListTypes {
+  createdAt: string
+  point: number
+  type: string
+  totalPoint: number
+}
