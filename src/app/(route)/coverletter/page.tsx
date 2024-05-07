d744fd3a318a6e4493ae8c9fe5c74eee35f8c4b7
김낙도 <tpcmsk0920@gmail.com>
2024-05-07 15:37:41 +0900

    🐛 [fix]: 빌드오류

--- src/app/(route)/coverletter/page.tsx
+++ src/app/(route)/coverletter/page.tsx
@@ -1,18 +1,13 @@
 'use client'
 
+import Header from '@/app/components/layout/Header'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
-import { useRouter } from 'next/navigation'
 import { covletNum } from '../../recoil/coverletter'
-import Header from '@/app/components/layout/Header'
 // import LoginContainer from '@/app/components/login/LoginContainer'
-import ExpInfoContainer from '@/app/components/experience/ExpInfoContainer'
-import ExpKeywordContainer from '@/app/components/experience/ExpKeywordContainer'
-import ExpContentContainer from '@/app/components/experience/ExpContentContainer'
 import CovletMain from '@/app/components/coverletter/CovletMain'
 import CovletSave from '@/app/components/coverletter/CovletSave'
-import CovletAiFeed from '@/app/components/coverletter/CovletAiFeed'
-import Footer from '@/app/components/layout/Footer'
 
 export default function CovletMainPage() {
   const [covletNumber, setCovletNumber] = useRecoilState(covletNum)
