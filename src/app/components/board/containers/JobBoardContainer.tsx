81fc7736877a20b30b6259c9a06e4671fbcdd6b2
Minkyu0424 <yng1404@gmail.com>
2024-05-09 17:07:05 +0900

    :sparkles:[feat]: 검색 게시물 없을 경우 화면

--- src/app/components/board/containers/JobBoardContainer.tsx
+++ src/app/components/board/containers/JobBoardContainer.tsx
@@ -6,11 +6,18 @@ interface JobBoardContainerProps {
 
 const JobBoardContainer = ({ boardData }: JobBoardContainerProps) => {
   return (
-    <div className="flex w-[783px] flex-wrap gap-x-[23px] gap-y-[30px]">
-      {boardData.list.map((post) => (
-        <JobPost data={post} />
-      ))}
-    </div>
+    <div>
+    {boardData.list.length > 0 ? (
+      <div className="flex w-[783px] flex-wrap gap-x-[23px] gap-y-[30px]">
+        {boardData.list.map((post) => <JobPost data={post} />)}
+      </div>
+    ) : (
+      <div className="text-2xl font-bold pt-[200px] w-full h-full flex items-center justify-center text-slate-700">
+        <div className="text-4xl">😵</div>
+        해당 검색어와 관련된 게시물이 존재하지 않습니다
+      </div>
+    )}
+  </div>
   )
 }
 
