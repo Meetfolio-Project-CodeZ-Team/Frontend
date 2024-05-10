bf40ea0c5cfe3edf6a339edd93a8c802fd47e076
Minkyu0424 <yng1404@gmail.com>
2024-05-10 15:30:25 +0900

    :sparkles:[feat]: 관리자 커뮤니티 타입에 따른 조회

--- src/app/components/admin/board/ComunityBoard.tsx
+++ src/app/components/admin/board/ComunityBoard.tsx
@@ -3,9 +3,18 @@ import ComunityInfo from './ComunityInfo'
 
 interface ComunityBoardProps {
   boardList: ResponseBoardData[]
+  boardType: string
 }
 
-const ComunityBoard = ({ boardList }: ComunityBoardProps) => {
+const ComunityBoard = ({ boardList, boardType }: ComunityBoardProps) => {
+  let filteredBoardList: ResponseBoardData[] = boardList
+
+  if (boardType !== '') {
+    filteredBoardList = boardList.filter(
+      (Board) => Board.boardType === boardType,
+    )
+  }
+
   return (
     <div className="flex flex-col w-[1034px] h-[720px] mt-[22px]">
       <div className="flex w-[1034px] h-[50px] pl-[38px] border-y border-[#616161] items-center text-black text-lg">
@@ -15,8 +24,9 @@ const ComunityBoard = ({ boardList }: ComunityBoardProps) => {
         <div className="ml-[178px]">{COMUNITY_BOARD_H[3]}</div>
         <div className="ml-[198px]">{COMUNITY_BOARD_H[4]}</div>
       </div>
-      {boardList.map((Board, i) => (
+      {filteredBoardList.map((Board, i) => (
         <ComunityInfo
+          key={i}
           createdAt={Board.createdAt}
           memberName={Board.memberName}
           title={Board.title}
--- src/app/components/admin/containers/BoardContainer.tsx
+++ src/app/components/admin/containers/BoardContainer.tsx
@@ -11,32 +11,21 @@ interface BoardContainerProps {
 
 const BoardContainer = ({ boardList }: BoardContainerProps) => {
   const [boardData, setBoardData] = useState<ResponseBoardData[]>([])
-  console.log(boardData, '가져온 게시판 데이터')
+  const [boardType, setBoardType] = useState('')
+  console.log(boardType, '선택한 게시판 타입')
 
   useEffect(() => {
     setBoardData(boardList)
   }, [boardList])
-
-  const getKeywordBoard = async (selectedBoard: string) => {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_NEXT_SERVER}/api/admin/board?keyword=${selectedBoard}`,
-    )
-    const boardData = await response.json()
-    setBoardData(boardData.result)
-  }
 
   return (
     <div className="flex flex-col gap-y-9 bg-white w-[full] pl-[54px] pt-[27px] pb-[60px]">
       <div className="text-[32px] font-bold leading-[48px]">커뮤니티 관리</div>
       <div className="flex items-center w-[1013px] justify-between">
         <SearhBoard searchBoard={setBoardData} />
-        <DropDownB
-          options={Board}
-          title={'전체'}
-          onSelect={() => console.log('클릭')}
-        />
+        <DropDownB options={Board} title={'전체'} setBoardType={setBoardType} />
       </div>
-      <ComunityBoard boardList={boardData} />
+      <ComunityBoard boardList={boardData} boardType={boardType} />
     </div>
   )
 }
