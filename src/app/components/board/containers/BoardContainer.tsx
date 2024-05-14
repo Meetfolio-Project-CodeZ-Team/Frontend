354e81c58813af750259f43ab8d9f498cbd6a3ee
Minkyu0424 <yng1404@gmail.com>
2024-05-14 21:45:46 +0900

    :recycle:[chore]: 글쓰기에 아이콘 축

--- src/app/components/board/containers/BoardContainer.tsx
+++ src/app/components/board/containers/BoardContainer.tsx
@@ -1,9 +1,9 @@
 'use client'
 import { boardDataState } from '@/app/recoil/board'
+import Pencil from '@/app/ui/svg/Pencil'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { useRecoilState } from 'recoil'
-import Button from '../../common/Button'
 import BoardHeader from '../BoardHeader'
 import GroupBoardContainer from './GroupBoardContainer'
 import JobBoardContainer from './JobBoardContainer'
@@ -28,15 +28,16 @@ const BoardContainer = ({ nickname, profile }: BoardContainerProps) => {
         <GroupBoardContainer boardData={boardData} />
       )}
 
-      <div className="flex w-full flex-row-reverse pt-6 pr-12 absolute top-[880px] right-6">
-        <Button
-          buttonText={'글쓰기'}
-          type={'addBoardBtn'}
-          isDisabled={false}
-          onClickHandler={() =>
+      <div className="flex w-full flex-row-reverse pt-6 pr-12 absolute top-[880px] right-6 cursor-pointer">
+        <div
+          className="flex items-center w-[90px] px-1 h-10 text-white font-semibold bg-[#486283] rounded-[10px] justify-center gap-x-2"
+          onClick={() =>
             router.push(`/board/post/${path}?nickname=${nickname}`)
           }
-        />
+        >
+          글쓰기
+          <Pencil />
+        </div>
       </div>
     </div>
   )
