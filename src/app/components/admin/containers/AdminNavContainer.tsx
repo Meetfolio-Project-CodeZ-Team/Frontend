175449cd3674a2c0769bea9bbb7e999e149da984
Minkyu0424 <yng1404@gmail.com>
2024-06-04 14:31:59 +0900

    :recycle: refactor: admin Nav 너비 조정

--- src/app/components/admin/containers/AdminNavContainer.tsx
+++ src/app/components/admin/containers/AdminNavContainer.tsx
@@ -7,12 +7,12 @@ interface AdminNavContainerProps {
 
 const AdminNavContainer = ({ selected }: AdminNavContainerProps) => {
   return (
-    <div className="flex flex-col w-[320px] pl-[53px] pt-[84px] shadow-lg bg-[#DEE5ED]">
+    <div className="flex flex-col w-[320px] pl-[53px] min-w-56 pt-[84px] shadow-lg bg-[#DEE5ED] min-h-[759px]">
       <div className="flex items-center mb-[48px] gap-x-2">
         <div className="w-12 h-12 rounded-[100px] text-4xl">👨‍💻</div>
         <div className="text-[25px] font-semibold">관리자</div>
       </div>
-      <div className="flex flex-col gap-y-8 text-[22px] font-normal">
+      <div className="flex flex-col gap-y-8 text-[22px] font-normal min-h-[509px]">
         <Link
           className={`${selected === 'dashboard' && 'font-bold'}`}
           href="/admin"
